69812f482a74642a1fb4c721e64f27134e786c11
Leon Casey <caseyl2012@gmail.com>
2018-11-10 14:42:43 +0000

    Initial commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        if (collisionInfo.collider.name == "Ground")
+        {
+            FindObjectOfType<PlayerMovement>().isGrounded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 2000f;
+    public bool moveLeft = false;
+    public bool moveRight = false;
+    //public float jumpHeight = 8f;
+    //public bool isGrounded = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        //if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    rb.AddForce(0, jumpHeight * Time.deltaTime, 0, ForceMode.VelocityChange);
+        //}
+    }
+}
